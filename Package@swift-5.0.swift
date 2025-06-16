9389ae138560f0548d455b74f224177e26d346df
piyushmathur-moz <113369448+piyushmathur-moz@users.noreply.github.com>
2025-06-16 17:59:19 +0530

    Updated library files to 1.2.0 without causing issue to the existing working (#5)
    * Added relevant changes of the latest version 1.2.0 of the library.
    
    * Replacing old library files with new one.

--- Package@swift-5.0.swift
+++ Package@swift-5.0.swift
@@ -6,9 +6,9 @@ let package = Package(
     name: "libPhoneNumber",
     platforms: [
         .macOS(.v10_10),
-        .iOS(.v9),
-        .tvOS(.v9),
-        .watchOS(.v2)
+        .iOS(.v11),
+        .tvOS(.v11),
+        .watchOS(.v4)
     ],
     products: [
         .library(
@@ -27,6 +27,7 @@ let package = Package(
             ],
             linkerSettings: [
                 .linkedFramework("CoreTelephony", .when(platforms: [.iOS, .macOS])),
+                .linkedFramework("Contacts", .when(platforms: [.iOS, .macOS, .watchOS])),
             ]
         ),
         .testTarget(
